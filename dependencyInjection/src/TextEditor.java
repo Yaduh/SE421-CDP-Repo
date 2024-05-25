336bfeca85fa7e4d9342878b80f92cc3731398b8
Yaduh <137622535+Yaduh@users.noreply.github.com>
2024-05-25 20:27:49 +0300

    Dependency Injection part 4

--- dependencyInjection/src/TextEditor.java
+++ dependencyInjection/src/TextEditor.java
@@ -1,11 +1,12 @@
 import com.google.inject.Inject;
+import com.google.inject.Key;
 
 public class TextEditor {
     private String content;
     private SpellChecker spellService;
 
     @Inject
-    public TextEditor(SpellChecker checker){
+    public TextEditor(SpellChecker checker, Key k){
         this.spellService = checker;
     }
     public void render(){
--- dependencyInjection/src/TextEditorModule.java
+++ dependencyInjection/src/TextEditorModule.java
@@ -1,11 +1,33 @@
+import java.util.Random;
+
 import com.google.inject.AbstractModule;
+import com.google.inject.Key;
+import com.google.inject.Provides;
+import com.google.inject.Singleton;
 
 public class TextEditorModule extends AbstractModule {
 
         @Override
         protected void configure(){
-            bind(SpellChecker.class).to(WindowsSpellChecker.class);
-            
+            bind(SpellChecker.class)
+            .to(WindowsSpellChecker.class) 
+            .in(Singleton.class);
+        }
+
+        @Provides
+        static Key generateKey(){
+            Random r = new Random();
+            return new Key("Win" + r.nextInt());
+        }
+        
+        @Singleton
+        @Provides
+        static SpellChecker provideSpellChecker(){
+            if(getOSName().equals("Win")){
+                return new WindowsSpellChecker();
+            }else if(getOSName().equals("MacOS")){
+                return new MacOsSpellChcker();
+            }
         }
     }
 
